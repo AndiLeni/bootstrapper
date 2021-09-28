b5d4fdc41c749367598ff5b995abf72b8c0dcebc
andileni <andi.lenhardt@googlemail.com>
2021-09-28 14:17:38 +0200

    add option to use file upload

--- pages/upload.php
+++ pages/upload.php
@@ -1,18 +1,17 @@
 <?php
 
-
 if (rex_request_method() == 'get') {
 
     echo rex_view::info('Hinweis: dieser Installer installiert die neueste nicht-beta Version eines Addons. Nutzen Sie den Installer für spezifische Versionen.');
 
-    $form = '<p>Hier bitte zeilenweise ein Addon zur Installation eintragen:</p>';
-    $form .= '<form method="POST">';
+
+    $form = '<p>Hier kann eine Datei mit zeilenweise eingetragenen Addons hochgeladen werden:</p>';
+    $form .= '<form method="POST" enctype="multipart/form-data">';
     $form .= '<div class="form-group">';
-    $form .= '<textarea class="form-control" name="packages" rows="20"></textarea>';
+    $form .= '<input class="form-control" name="packages" type="file">';
     $form .= '</div>';
     $form .= '<button type="submit" class="btn btn-primary">Installieren</button>';
     $form .= '</form>';
-
 
     $fragment = new rex_fragment();
     $fragment->setVar('class', 'edit', false);
@@ -20,15 +19,21 @@ if (rex_request_method() == 'get') {
     $fragment->setVar('body', $form, false);
     echo $fragment->parse('core/page/section.php');
 } elseif (rex_request_method() == 'post') {
+    $file = rex_files('packages', 'array', []);
+    
 
-    $packages = rex_post('packages', 'string', '');
-
-    if ($packages == '') {
+    if ($file == []) {
         echo rex_view::error('Keine Pakete angegeben.');
+        return;
     } else {
-        $packages_to_install = explode("\n", str_replace("\r", "", $packages));
+
+        $file_content = file_get_contents($file['tmp_name']);
+        
+        $packages_to_install = explode("\n", str_replace("\r", "", $file_content));
+
         echo '<h2>Ausgewählte Addons:</h2>';
         dump($packages_to_install);
+
     }
 
     // $packages_to_install = ['forcal', 'watson', 'mform', 'mblock', 'yrewrite', 'yform', 'yform_spam_protection', 'yrewrite_scheme', 'adminer', 'uikit_collection'];
